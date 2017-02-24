a9266597653c946a3f5dd60d6913c3745338e792
Tom Steele <thomasjsteele@gmail.com>
2017-02-24 01:28:40 -0700

    Adds job sub command

--- cmd/job.go
+++ cmd/job.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"strconv"
+
 	"github.com/spf13/cobra"
+	hashstack "github.com/stricture/hashstack-server-core-ng"
 )
 
 var (
@@ -33,13 +37,111 @@ var jobCmd = &cobra.Command{
 	},
 }
 
+type jobRequest struct {
+	Name                string `json:"name"`
+	ListID              int64  `json:"list_id"`
+	AttackID            int64  `json:"attack_id"`
+	Priority            int    `json:"priority"`
+	MaxDedicatedDevices int    `json:"max_dedicated_devices"`
+	OpenCLVectorWidth   int    `json:"opencl_vector_width"`
+}
+
+type attackStep struct {
+	IDX                   int    `json:"idx"`
+	AttackMode            int    `json:"attack_mode"`
+	WordlistID            int64  `json:"wordlist_id"`
+	WordlistCombinationID int64  `json:"wordlist_combination_id"`
+	RuleID                int64  `json:"rule_id"`
+	RuleBufLeft           string `json:"rule_buf_left"`
+	RuleBufRight          string `json:"rule_buf_right"`
+	Mask                  string `json:"mask"`
+	IsHexCharset          bool   `json:"is_hex_charset"`
+	MarkovThreshold       int    `json:"markov_threshold"`
+	MarkovHCStatFileID    int64  `json:"markov_hc_stat_file_id"`
+	CustomCharset1        string `json:"custom_charset1"`
+	CustomCharset2        string `json:"custom_charset2"`
+	CustomCharset3        string `json:"custom_charset3"`
+	CustomCharset4        string `json:"custom_charset4"`
+}
+
+type attackRequest struct {
+	Title string       `json:"title"`
+	Steps []attackStep `json:"steps"`
+}
+
 var newJobCmd = &cobra.Command{
-	Use:    "new project_id list_id [dictionary|mask]",
+	Use:    "new <project_id> <list_id> <name> <dictionary|mask>",
 	Short:  "Create a new job for the provided project and list",
 	Long:   "Create a new job for the provided project and list",
 	PreRun: ensureAuth,
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 4 {
+			writeStdErrAndExit("missing required arugment")
+		}
+		var (
+			projectIDStr = args[0]
+			listIDStr    = args[1]
+			name         = args[2]
+		)
+		projectID, err := strconv.Atoi(projectIDStr)
+		if err != nil {
+			writeStdErrAndExit("invalid project_id")
+		}
+		listID, err := strconv.Atoi(listIDStr)
+		if err != nil {
+			writeStdErrAndExit("invalid list_id")
+		}
+		step := attackStep{
+			IDX:        0,
+			AttackMode: flAttackMode,
+		}
+		switch flAttackMode {
+		case 0:
 
+		case 1:
+
+		case 3:
+			step.Mask = args[3]
+			step.CustomCharset1 = flCustomCharset1
+			step.CustomCharset2 = flCustomCharset2
+			step.CustomCharset3 = flCustomCharset3
+			step.CustomCharset4 = flCustomCharset4
+			step.IsHexCharset = flIsHexCharset
+		case 6:
+
+		case 7:
+
+		default:
+			writeStdErrAndExit("invalid attack-mode")
+		}
+		attack := attackRequest{
+			Title: fmt.Sprintf("hashstack-cli-%d-%d-%s", projectID, listID, name),
+			Steps: []attackStep{step},
+		}
+		data, err := postJSON("/api/attacks", &attack)
+		if err != nil {
+			writeStdErrAndExit(err.Error())
+		}
+		debug("uploaded temporary attack plan")
+		var plan hashstack.Attack
+		if err := json.Unmarshal(data, &plan); err != nil {
+			debug(err.Error())
+			writeStdErrAndExit("there was an error decoding a response from the server")
+		}
+
+		job := jobRequest{
+			Name:                name,
+			ListID:              int64(listID),
+			AttackID:            plan.ID,
+			Priority:            flPriority,
+			MaxDedicatedDevices: flMaxDedicatedDevices,
+			OpenCLVectorWidth:   flOpenCLVectorWidth,
+		}
+		data, err = postJSON(fmt.Sprintf("/api/projects/%s/jobs", projectIDStr), &job)
+		if err != nil {
+			writeStdErrAndExit(err.Error())
+		}
+		fmt.Println(string(data))
 	},
 }
 
