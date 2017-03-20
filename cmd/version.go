ab3c1a211076ef85c2b4bf6211ed75279696938c
Tom Steele <thomasjsteele@gmail.com>
2017-03-20 12:12:22 -0700

    Get server version

--- cmd/version.go
+++ cmd/version.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -12,11 +15,51 @@ func init() {
 
 var version = "1.0.0"
 
+type serverVersion struct {
+	Version string `json:"version"`
+}
+
+func getServerVersion() (string, error) {
+	debug(fmt.Sprintf("HTTP: GET %s", "/version"))
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s", flServerURL, "/version"), nil)
+	if err != nil {
+		debug(fmt.Sprintf("Error: %s", err.Error()))
+		return "", new(requestCreateError)
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		debug(fmt.Sprintf("Error: %s", err.Error()))
+		return "", new(requestError)
+	}
+	if err := statusToError(resp.StatusCode); err != nil {
+		return "", err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		debug(fmt.Sprintf("Error: %s", err.Error()))
+		return "", new(invalidResponseError)
+	}
+	var version serverVersion
+	if err := json.Unmarshal(body, &version); err != nil {
+		debug(fmt.Sprintf("Error: %s", err.Error()))
+		return "", new(jsonServerError)
+	}
+	return version.Version, nil
+}
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version and exit",
-	Long:  "Print the version and exit",
+	Short: "Print client and server version and exit",
+	Long:  "Print client and server version and exit",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Version: %s", version)
+		fmt.Printf("Client Version: %s\n", version)
+		serverv, err := getServerVersion()
+		if err != nil {
+			writeStdErrAndExit(err.Error())
+			return
+		}
+		fmt.Printf("Server Version: %s\n", serverv)
 	},
 }
