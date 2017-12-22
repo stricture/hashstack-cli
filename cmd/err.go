a4d46a60e36be003c35ba5bc33a8f06f0fe715ab
Tom Steele <thomasjsteele@gmail.com>
2017-12-22 09:41:38 -0800

    Updates email address of support

--- cmd/err.go
+++ cmd/err.go
@@ -8,6 +8,6 @@ import (
 func writeStdErrAndExit(msg string) {
 	fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n", msg))
 	fmt.Fprintf(os.Stderr, "\nRunning with --debug will show additional context for this error.\n")
-	fmt.Fprintf(os.Stderr, "\nEmail support@sagitta.pw for more information. Please include --debug output!\n")
+	fmt.Fprintf(os.Stderr, "\nEmail support@terahash.com for more information. Please include --debug output!\n")
 	os.Exit(1)
 }
