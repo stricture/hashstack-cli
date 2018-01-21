6a34de4ff919a3a81d5cf0a456d9052c102e71e3
Tom Steele <thomasjsteele@gmail.com>
2018-01-21 18:00:04 -0500

    Only show debug help if debug is not enabled

--- cmd/err.go
+++ cmd/err.go
@@ -7,7 +7,9 @@ import (
 
 func writeStdErrAndExit(msg string) {
 	fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n", msg))
-	fmt.Fprintf(os.Stderr, "\nRunning with --debug will show additional context for this error.\n")
+	if !flDebug {
+		fmt.Fprintf(os.Stderr, "\nRunning with --debug will show additional context for this error.\n")
+	}
 	fmt.Fprintf(os.Stderr, "\nEmail support@terahash.com for more information. Please include --debug output!\n")
 	os.Exit(1)
 }
